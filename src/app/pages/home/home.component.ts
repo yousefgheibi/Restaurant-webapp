e495e870f46e86f2e169b685875f8594224106b1
Yousef <gheibiyousef2000@gmail.com>
2022-03-27 19:19:21 +0430

    add blog to home page with api

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogModel } from 'src/app/models/blog.model';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  blogData : BlogModel[] = [];
+  constructor(private _blogapi : BlogService) {}
 
   ngOnInit(): void {
+    this.getBlog();
   }
-
-}
+  getBlog() {
+    this._blogapi.getBlogs().subscribe((res) => {
+      this.blogData = res;
+    });
+  }
+} 
