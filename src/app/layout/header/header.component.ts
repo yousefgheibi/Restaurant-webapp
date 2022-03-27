84c8de3ad82f46f80228b4b4f5b93bd5e854899f
Yousef <gheibiyousef2000@gmail.com>
2022-03-27 15:42:19 +0430

    refactor menu button

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -6,22 +6,23 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  isLoggedIn = true;
   @ViewChild('aside') sidebar!: ElementRef;
 
   constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
     document.getElementById('close-btn')?.addEventListener('click', () => {
-        this.renderer.setStyle(this.sidebar.nativeElement,"display","none");
-  })
+      this.renderer.setStyle(this.sidebar.nativeElement, 'display', 'none');
+    });
 
-  document.getElementById('menu-btn')?.addEventListener('click', () => {
-    this.renderer.setStyle(this.sidebar.nativeElement, 'display', 'flex');
-  });
+    document.getElementById('menu-btn')?.addEventListener('click', () => {
+      console.log('clicked');
+      this.renderer.setStyle(this.sidebar.nativeElement, 'display', 'flex');
+    });
+  }
 
-}
-
-onCloseMenu(){
-this.renderer.setStyle(this.sidebar.nativeElement, 'display', 'none');
-}
+  onCloseMenu() {
+    this.renderer.setStyle(this.sidebar.nativeElement, 'display', 'none');
+  }
 }
